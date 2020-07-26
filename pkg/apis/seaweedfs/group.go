0c28e89258632ae54fe3f779f2d39b300813a775
Chris Lu <chris.lu@gmail.com>
2020-07-26 14:47:24 -0700

    regenerate with operator-sdk 3-alpha

--- pkg/apis/seaweedfs/group.go
+++
@@ -1,6 +0,0 @@
-// Package seaweedfs contains seaweedfs API versions.
-//
-// This file ensures Go source parsers acknowledge the seaweedfs package
-// and any child packages. It can be removed if any other Go source files are
-// added to this package.
-package seaweedfs

b08d5ce9d1ebc19375add95ac650d1ed846537f1
Chris Lu <chris.lu@gmail.com>
2021-05-01 01:08:10 -0700

    2.43

--- controllers/seaweed_controller_test.go
+++ controllers/seaweed_controller_test.go
@@ -39,7 +39,7 @@ var _ = Describe("Seaweed Controller", func() {
 						Name:      name,
 					},
 					Spec: seaweedv1.SeaweedSpec{
-						Image:                 "chrislusf/seaweedfs:2.42",
+						Image:                 "chrislusf/seaweedfs:2.43",
 						VolumeServerDiskCount: 1,
 						Master: &seaweedv1.MasterSpec{
 							Replicas:        3,

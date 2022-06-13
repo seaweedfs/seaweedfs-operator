803e1a2021dc6e3411c1e83d33f6debbcb91f450
chrislu <chris.lu@gmail.com>
2022-06-12 19:30:52 -0700

    3.10

--- README.md
+++ README.md
@@ -88,7 +88,7 @@ metadata:
   namespace: default
 spec:
   # Add fields here
-  image: chrislusf/seaweedfs:3.09
+  image: chrislusf/seaweedfs:3.10
   volumeServerDiskCount: 1
   hostSuffix: seaweed.abcdefg.com
   master:
--- controllers/seaweed_controller_test.go
+++ controllers/seaweed_controller_test.go
@@ -39,7 +39,7 @@ var _ = Describe("Seaweed Controller", func() {
 						Name:      name,
 					},
 					Spec: seaweedv1.SeaweedSpec{
-						Image:                 "chrislusf/seaweedfs:3.09",
+						Image:                 "chrislusf/seaweedfs:3.10",
 						VolumeServerDiskCount: 1,
 						Master: &seaweedv1.MasterSpec{
 							Replicas:        3,
